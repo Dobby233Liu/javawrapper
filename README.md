6bed88753e2dced2a9bd828a997d31f8babd6814
Liu Wenyuan <15816141883@163.com>
2023-04-15 00:48:27 +0800

    Probably good enough now

---
+++ README.md
@@ -0,0 +1,2 @@
+Please don't actually use this for anything.
+I was just trying to run [Façade](https://en.wikipedia.org/wiki/Fa%C3%A7ade_(video_game))'s backend with Eclipse Temurin's Java 17 RE.
--- src/bin/java.rs
+++ src/bin/java.rs
@@ -1,10 +1,11 @@
 use std::env;
 use std::process;
+/*use std::fs::File;*/
 
 use quit;
 use confy;
 
-use msgbox;
+/*use msgbox;*/
 
 #[derive(::serde::Serialize, ::serde::Deserialize)]
 struct WrapperConfig {
@@ -14,6 +15,11 @@ impl ::std::default::Default for WrapperConfig {
     fn default() -> Self { Self { orig_exe: "java_".into() } }
 }
 
+fn prevent_obsolete_params(v : &String) -> bool {
+    // Very rough
+    v != "-Xfuture" && v != "-Xincgc"
+}
+
 #[quit::main]
 fn main() -> Result<(), ::std::io::Error> {
     let cfg : WrapperConfig = confy::load("javawrapper")?;
@@ -21,6 +27,7 @@ fn main() -> Result<(), ::std::io::Error> {
     let mut our_args : Vec<String> = env::args().collect();
 
     // Debug provided args
+    /*
     match msgbox::create(
         "jw",
         &our_args.join(" "),
@@ -29,15 +36,27 @@ fn main() -> Result<(), ::std::io::Error> {
         Ok(()) => {},
         Err(_) => quit::with_code(1)
     };
+    */
 
     let my_full_path_b = env::current_exe()?;
     let my_full_path = my_full_path_b.as_path();
     let exe_path = my_full_path.parent().unwrap();
 
+    // Debug out
+    /*
+    let out_file = File::create("java.log")?;
+    let out_file_e = out_file.try_clone()?;
+    */
+
     let mut java_exe = process::Command::new(
         exe_path.join(cfg.orig_exe)
     );
-    java_exe.args(our_args.drain(1..));
+    java_exe.args(our_args.drain(1..).filter(prevent_obsolete_params))
+    /*
+            .stdout(process::Stdio::from(out_file))
+            .stderr(process::Stdio::from(out_file_e))
+    */
+    ;
 
     quit::with_code(match java_exe.status() {
         Ok(status) => {
